7960cc058c04230b1f468b44fad55de031e106eb
Felix Ayala <felix1262@gmail.com>
2015-06-05 16:38:58 -0400

    Initial Commit

---
+++ MagicCarpetView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicCarpetView
+//
+//  Created by Felix Ayala on 6/5/15.
+//  Copyright (c) 2015 Pandorga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
